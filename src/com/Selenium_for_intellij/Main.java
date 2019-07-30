0c2b713ffdbdd10b6c3f31dee75dd8b59b367fa9
UmarNasib <umarnasib13@gmail.com>
2019-07-30 10:49:58 +0600

    A sample project for login into Gmail & Facebook

--- src/com/Selenium_for_intellij/Main.java
+++ src/com/Selenium_for_intellij/Main.java
@@ -6,30 +6,52 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
+import java.lang.Exception;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         login_to_gmail();
         login_to_facebook();
     }
 
-    private static void login_to_gmail(){
-        System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Gecko Driver\\Chorom\\chromedriver_win32\\chromedriver.exe");
-        WebDriver obj = new ChromeDriver();
-        obj.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        obj.get("https://www.gmail.com");
-        obj.findElement(By.id("identifierId")).sendKeys("youremailaddress@xyz.com", Keys.ENTER);
-        obj.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("your@pass", Keys.ENTER);
-       }
+    public static Object Login() throws Exception {
+        try {
+            System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Gecko Driver\\Chorom\\chromedriver_win32\\chromedriver.exe");
+            WebDriver obj = new ChromeDriver();
+            obj.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+            obj.manage().window().maximize();
+            return obj;
+        }
+        catch (Exception e){
+            System.out.println("Something went wrong in your Login() Method");
+            return null;
+        }
+    }
 
-    private static void login_to_facebook(){
-        System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Gecko Driver\\Chorom\\chromedriver_win32\\chromedriver.exe");
-        WebDriver obj = new ChromeDriver();
-        obj.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        obj.get("https://www.facebook.com/aunasib");
-        obj.findElement(By.id("email")).sendKeys("youremailaddress@xyz.com");
-        obj.findElement(By.id("pass")).sendKeys("your@pass", Keys.ENTER);
-        obj.findElement(By.xpath("//*[@id=\"u_0_2\"]")).click();
+    public static void login_to_gmail() throws Exception {
+        try {
+            WebDriver obj = (WebDriver) Login();
+            obj.get("https://www.gmail.com");
+            obj.findElement(By.id("identifierId")).sendKeys("youremailaddress@xyz.com", Keys.ENTER);
+            obj.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input")).sendKeys("your@pass", Keys.ENTER);
+        }
+        catch (Exception e){
+            System.out.println("Something went wrong in your login_to_gmail() Method");
+        }
+    }
+
+    public static void login_to_facebook() throws Exception {
+        try {
+            WebDriver obj = (WebDriver) Login();
+
+            obj.get("https://www.facebook.com/aunasib");
+            obj.findElement(By.id("email")).sendKeys("youremailaddress@xyz.com");
+            obj.findElement(By.id("pass")).sendKeys("your@pass", Keys.ENTER);
+            obj.findElement(By.xpath("//*[@id=\"u_0_2\"]")).click();
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong in your login_to_facebook() Method");
+        }
     }
 }
